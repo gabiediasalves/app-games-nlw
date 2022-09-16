7c78923471e03960094c0f31ecf5f5491a1d14c7
gabiediasalves <gabiediasalves@gmail.com>
2022-09-15 22:19:09 -0300

    updating mobile

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -73,8 +73,8 @@ export function App() {
           <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
           </div>
 
-          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded'>
-            <MagnifyingGlassPlus/>
+          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24}/>
             Publicar anúncio
             </button>
         </div>
